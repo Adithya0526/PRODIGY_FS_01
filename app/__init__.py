65fb46c38eb07133281656b14e57d651839ec44a
Adithya0526 <sadithya.s256@gmail.com>
2025-01-05 13:06:46 +0530

    Verison 1

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from config import Config
+from app.config import Config
 
 db=SQLAlchemy()
 bcrypt=Bcrypt()
@@ -11,15 +11,20 @@ login_manager.login_view='main.login'
 login_manager.login_message_category='info'
 
 def create_app():
-    app=Flask(__name__)
-    app.config.from_object('Config')
+    app = Flask(__name__)
+    app.config.from_object(Config)  # Correctly referencing the Config class
+
+    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
-    
+    # Register blueprints
     from app.routes import main
     app.register_blueprint(main)
-    db.create_all()
-    
+
+    # Create database tables within app context
+    with app.app_context():
+        db.create_all()
+
     return app
